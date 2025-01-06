8c15236b5379f6288c41494eea18be5018094290
Lesyalys <nya.olesya@bk.ru>
2025-01-06 21:43:52 +0500

    new

---
+++ fullScreen.js
@@ -0,0 +1,10 @@
+(function() {
+    const videoElement = document.querySelector('.video-stream.html5-main-video');
+    if (videoElement) {
+  videoElement.style.width = '100vw';
+  videoElement.style.height = '100vh';
+  videoElement.style.objectPosition = 'center';
+  videoElement.style.top = '0px';
+  videoElement.style.left = '0px';
+    }
+  })();
