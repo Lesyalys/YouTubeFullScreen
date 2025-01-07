9ec53a19ace19a28fa44a2e399d9c44ea38c3f17
Lesyalys <nya.olesya@bk.ru>
2025-01-07 14:35:34 +0500

    new

--- fullScreen.js
+++ fullScreen.js
@@ -1,6 +1,8 @@
+//BETA
 (function() {
     const videoElement = document.querySelector('.video-stream.html5-main-video');
     if (videoElement) {
+      
   videoElement.style.width = '100vw';
   videoElement.style.height = '100vh';
   videoElement.style.objectPosition = 'center';
